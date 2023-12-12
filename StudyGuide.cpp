52ea5726d64861c210a8def2704b8d4ab4c5f05f
Arron Dick <arronmdick@gmail.com>
2023-12-11 20:44:17 -0600

    containers, loops, and classes

--- StudyGuide.cpp
+++ StudyGuide.cpp
@@ -1,6 +1,8 @@
 // File for refresher 
 #include <iostream> //files useful for input and output to console
 #include <string>
+#include <vector>
+#include <array>
 
 using namespace std; //includes entire std library; warning:updates in std can cause program errors
 
@@ -21,7 +23,7 @@ int main() // begin program
     unsigned long uLongNum{};
     unsigned long long uLongLongNum{};
     char Char{'y'}; //single quote for single char
-    string String{"it be like that sometimes"};
+    string String{"it be like that"};
     auto autoIntNum{32}; //compiler will declare variable with automatic type deduction.
     bool dayTime{}; //default value is true
 
@@ -48,7 +50,7 @@ int main() // begin program
     NarcanVolume = static_cast<int>(NarcanDose);
     
     int autoStorage{}; //autoStorage has automatic storage duration meaning it only exists within a set of brackets
-    static int staticStorage{}; //staticStorage has static storage so it exists until the program ends
+    //static int staticStorage{}; //staticStorage has static storage so it exists until the program ends
     thread_local int threadStorage{}; //threadStorage has thread storage duration so each thread can have it own instance of threadStorage.
 
     //<=> 3 way comparison introduced in C20. returns 0 if values are equal, -1 if 1st is < 2nd, or 1 if 1st > 2nd.
@@ -58,16 +60,58 @@ int main() // begin program
 
 
     //ternary operators: condition ? result_if_true : result if false. shorthand if-else statement
-    shortNum > intNum ? cout << shortNum : cout << intNum;
+    shortNum > intNum ? cout << shortNum : cout << intNum << endl;
 
-    switch(shortNum)
+    switch(shortNum) //only used with ints and enums
     {
-        case 1: cout << "Yes it's " << shortNum;
+        case 1: cout << "Yes it's " << shortNum << endl;
         break; //break signals to exit the switch statement
 
-        default: cout << "not shortNum";
+        default: cout << "not shortNum" << endl;
         break;
     }
-    
+
+
+    for (char i : String) // range based for loop. interate through string and stores the elements in i;
+    {
+        cout << shortNum << endl;
+    }
+
+    while(shortNum < 100)
+    {
+        cout << shortNum;
+        shortNum++;
+    }
+
+    int studyArray[2][2] {{1,2},{3,4}}; //multidimensional array
+
+    array<int,3>stdarray {1,2,3}; //std array
+
+    vector <int> myVector {1,2,3,4,5}; //dynamic sizing and remember their size
+
+    vector<vector<int>> vector2D (3, vector<int>(10)); //parenthesis initialize the vector; 3 rows and 10 columns of vectors
+    vector2D[1][2] = 42;
+
+    cout << String.length() << endl;
+    cout << String.append(" sometimes") << endl;
+
+    //variations of random number generation
+    unsigned int Time = time(0);
+    srand(Time);
+    unsigned int rollNum = (rand() % 6) + 1;
+    cout << rollNum << endl;
+
+    class People
+    {
+    public:
+        string getHairColor() { return hairColor; }
+    private:
+        string hairColor{"brown"};
+
+    };
+
+    People Arron;
+    cout << Arron.getHairColor() << endl;
+
     return 0; //end program. compiler would also add by default
 }
